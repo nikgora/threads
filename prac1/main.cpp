893a551f504cc7f9e33485458eb653c73c6e2812
mykola <nik.gora.2004@gmail.com>
2024-02-19 12:32:57 +0100

    initial comit

--- prac1/main.cpp
+++ prac1/main.cpp
@@ -68,12 +68,7 @@ int main()
     std::thread thread122(run1,"second12");
     std::thread thread113(run1,"first13");
     std::thread thread123(run1,"second13");
-    thread111.join();
-    thread121.join();
-    thread112.join();
-    thread122.join();
-    thread113.join();
-    thread123.join();
+
     std::cout<<"\n\n\n";
     //TASK 2.1
     std::thread thread2111(run211,"first211", true);
@@ -82,12 +77,10 @@ int main()
     std::thread thread2122(run212,"second212", true);
     std::thread thread2113(run211,"first213", true);
     std::thread thread2123(run212,"second213", true);
-    thread2111.join();
-    thread2121.join();
-    thread2112.join();
-    thread2122.join();
-    thread2113.join();
-    thread2123.join();
+    std::thread thread2114(run211,"first214", true);
+    std::thread thread2124(run212,"second214", true);
+    std::thread thread2115(run211,"first215", true);
+    std::thread thread2125(run212,"second215", true);
 
     std::cout<<"\n\n\n";
     //TASK 2.2
@@ -95,14 +88,31 @@ int main()
     std::thread thread222(run22,"first222");
     std::thread thread223(run22,"first223");
 
-    thread221.join();
-    thread222.join();
-    thread223.join();
+
     std::cout<<"\n\n\n";
     //TASK 3
     std::vector<int> vector1 = {1, 2, 3, 4, 5};
     std::vector<int> vector2 = {6, 7, 8, 9, 4};
     std::thread thread3 (partialDotProduct, std::ref(vector1), std::ref(vector2));
+    thread111.join();
+    thread121.join();
+    thread112.join();
+    thread122.join();
+    thread113.join();
+    thread123.join();
+    thread2111.join();
+    thread2121.join();
+    thread2112.join();
+    thread2122.join();
+    thread2113.join();
+    thread2123.join();
+    thread2114.join();
+    thread2124.join();
+    thread2115.join();
+    thread2125.join();
+    thread221.join();
+    thread222.join();
+    thread223.join();
     thread3.join();
     return 0;
 }
